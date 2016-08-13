9c0b4f45fe4bd6c790d1546052404d70042ae46f
Sha-KB <sha@shianra.com>
2016-08-14 02:01:15 +1000

    Basic error handling.

--- js/mosaic.js
+++ js/mosaic.js
@@ -1,15 +1,24 @@
+var showError = function (message) {
+    var element = document.getElementById('error');
+    element.innerHTML = message;
+    element.className = 'visible';
+};
+
+var hideError = function () {
+    var element = document.getElementById('error');
+    element.className = 'hidden';
+};
+
 /**
  * @function photoMosaic
  */
-var photoMosaic = function ()
-{
+var photoMosaic = function () {
     'use strict';
 
     /**
      * @function renderMosaic
      */
-    var renderMosaic = function ()
-    {
+    var renderMosaic = function (tileWidth, tileHeight) {
         URL.revokeObjectURL(url);
 
         /**
@@ -29,21 +38,19 @@ var photoMosaic = function ()
         /**
          * @function renderByRow
          */
-        var renderByRow = function ()
-        {
-            if (imageRow < canvas.height)
-            {
+        var renderByRow = function () {
+            if (imageRow < canvas.height) {
                 requestAnimationFrame(renderByRow)
             }
-            inScreenContext.drawImage(canvas, 0, imageRow, canvas.width, TILE_HEIGHT, 0, imageRow, canvas.width, TILE_HEIGHT);
-            imageRow += TILE_HEIGHT;
+            inScreenContext.drawImage(canvas, 0, imageRow, canvas.width, tileHeight, 0, imageRow, canvas.width,
+                tileHeight);
+            imageRow += tileHeight;
         };
 
         /**
          * @function pushToDoc
          */
-        var pushToDoc = function ()
-        {
+        var pushToDoc = function () {
             document.getElementById('mosaicOutput').innerHTML = '';
             document.getElementById('mosaicOutput').appendChild(inScreen);
         };
@@ -53,11 +60,8 @@ var photoMosaic = function ()
          * tile. The browser's image smoothing assists with getting the average colour. However, this will only work
          * with more modern browsers.
          * @function colourTiles
-         * @param {Node} smallCanvas
-         * @param {CanvasRenderingContext2D} smallContext
          */
-        var colourTiles = function ()
-        {
+        var colourTiles = function () {
             context.globalCompositeOperation = 'source-in';
             var smallCanvas = canvas.cloneNode(true);
             smallCanvas.width = Math.floor(xTileCount);
@@ -68,7 +72,7 @@ var photoMosaic = function ()
             context.webkitImageSmoothingEnabled = false;
             context.msImageSmoothingEnabled = false;
             context.imageSmoothingEnabled = false;
-            context.scale(TILE_WIDTH, TILE_HEIGHT);
+            context.scale(tileWidth, tileHeight);
             context.drawImage(smallCanvas, 0, 0);
         };
 
@@ -76,12 +80,9 @@ var photoMosaic = function ()
          * @desc Generate an initial set of tiles to fill the image.
          * @function generateTiles
          */
-        var generateTiles = function ()
-        {
-            for (var y = 0; y < image.height; y += TILE_HEIGHT)
-            {
-                for (var x = 0; x < image.width; x += TILE_WIDTH)
-                {
+        var generateTiles = function () {
+            for (var y = 0; y < image.height; y += tileHeight) {
+                for (var x = 0; x < image.width; x += tileWidth) {
                     context.drawImage(svgTile, x, y)
                 }
             }
@@ -89,11 +90,9 @@ var photoMosaic = function ()
 
         /**
          * @desc Calculate the number of tiles there will be per row and column, then determine the final width and
-         * height (it differs from the original image because the width and/or height of the uploaded image may not be
-         * divisible by the given tile sixes). Then create an off-screen canvas and an on-screen canvas and begin
+         * height (it differs from the original image because the width and/or height of the uploaded image may not
+         * be divisible by the given tile sixes). Then create an off-screen canvas and an on-screen canvas and begin
          * generating the tiles as SVGs.
-         * @param {number} TILE_WIDTH
-         * @param {number} TILE_HEIGHT
          * @param {number} xTileCount The number of tiles that fit within the uploaded image on the X axis
          * @param {number} yTileCount The number of tiles that fit within the uploaded image on the Y axis
          * @param {number} outputWidth The width of the canvas that fits the entire mosaic.
@@ -106,12 +105,10 @@ var photoMosaic = function ()
          * @param svgShape
          * @param svgData
          */
-        var TILE_WIDTH = parseInt(document.getElementById('tileWidth').value);
-        var TILE_HEIGHT = parseInt(document.getElementById('tileHeight').value);
-        var xTileCount = Math.floor(image.width / TILE_WIDTH);
-        var yTileCount = Math.floor(image.height / TILE_HEIGHT);
-        var outputWidth = image.width - (image.width % TILE_WIDTH);
-        var outputHeight = image.height - (image.height % TILE_HEIGHT);
+        var xTileCount = Math.floor(image.width / tileWidth);
+        var yTileCount = Math.floor(image.height / tileHeight);
+        var outputWidth = image.width - (image.width % tileWidth);
+        var outputHeight = image.height - (image.height % tileHeight);
         var canvas = createCanvas(outputWidth, outputHeight);
         var context = canvas.getContext('2d');
         var inScreen = canvas.cloneNode(true);
@@ -119,11 +116,11 @@ var photoMosaic = function ()
         var imageRow = 0;
 
         var svgTile = new Image();
-        var svgTileData = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="' + TILE_WIDTH + '" height="' + TILE_HEIGHT + '">' +
+        var svgTileData = '<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="' + tileWidth + '" height="'
+            + tileHeight + '">' +
             '<ellipse cx="50%" cy="50%" rx="50%" ry="50%"></ellipse>' +
             '</svg>';
-        svgTile.onload = function ()
-        {
+        svgTile.onload = function () {
             generateTiles();
             colourTiles();
             pushToDoc();
@@ -138,14 +135,29 @@ var photoMosaic = function ()
      */
     var url = URL.createObjectURL(this.files[0]);
     var image = new Image();
-    image.onload = renderMosaic;
+    image.onload = function () {
+        /**
+         * @param {Number} tileWidth
+         * @param {Number} tileHeight
+         */
+        var tileWidth = parseInt(document.getElementById('tileWidth').value);
+        var tileHeight = parseInt(document.getElementById('tileHeight').value);
+        if (tileWidth <= 0 || tileHeight <= 0) {
+            showError('Tiles should be larger than 0.');
+        }
+        else if (tileWidth > image.width || tileHeight > image.height) {
+            showError('Individual tiles should not be larger than the image.');
+        } else {
+            hideError();
+            renderMosaic(tileWidth, tileHeight);
+        }
+    };
     image.src = url;
 };
 
 /**
  * @desc Image upload triggers photo mosaic process.
  */
-window.onload = function ()
-{
+window.onload = function () {
     document.getElementById('imageInput').onchange = photoMosaic
 };
